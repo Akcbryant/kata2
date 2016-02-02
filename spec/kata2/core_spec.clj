068260ac57c00f32b6942c4d942b743900ad0941
Kirby Bryant <akcbryant@gmail.com>
2016-02-02 12:45:49 -0800

    Finished the implementation

--- spec/kata2/core_spec.clj
+++ spec/kata2/core_spec.clj
@@ -10,8 +10,26 @@
   (it "2.3.4.5.6"
     (should= ["2" "3" "4" "5" "6"] (version-vec "2.3.4.5.6"))))
 
+(describe "Pad version vectors"
+  (it "[1 0 ] [1 0 0]"
+    (should= ["1" "0" "0"] (pad-vec ["1" "0"] ["1" "0" "0"])))
+  (it "[1] [1 2 3 4]"
+    (should= ["1" "0" "0" "0"] (pad-vec ["1"] ["1" "2" "3" "4"]))))
+
 (describe "Compare two vectors of numbers"
   (it "[1] [1]"
-    (should= 0 (compare-versions ["1"]["1"])))
+    (should= '(0) (compare-vecs ["1"]["1"])))
   (it "[1 0] [0 0 1]"
-    (should= 1 (compare-versions ["1" "0"]["0" "0" "1"]))))
+    (should= '(1 0) (compare-vecs ["1" "0"]["0" "0" "1"]))))
+
+(describe "Compare two version strings"
+  (it "1.2.3.4 and 2.3.4"
+    (should= -1 (version-compare "1.2.3.4" "2.3.4")))
+  (it "1 and 0"
+    (should= 1 (version-compare "1" "0")))
+  (it "0.0.0.1 and 0.1.0.2"
+    (should= -1 (version-compare "0.0.3.1" "0.1.0.2")))
+  (it "0.0.1 and 0.0.1"
+    (should= 0 (version-compare "0.0.1" "0.0.1")))
+  (it "1.2.3.4 and 1"
+    (should= 1 (version-compare "1.2.3.4" "1"))))
--- src/kata2/core.clj
+++ src/kata2/core.clj
@@ -4,7 +4,23 @@
 (defn version-vec [version-str]
   (string/split version-str #"\."))
 
-(defn compare-versions [version1 version2]
-  (map #(cond (> (read-string %1) (read-string %2)) 1
-              (= (read-string %1) (read-string %2)) 0
-              :else -1) version1 version2))
+(defn pad-vec [vec1 vec2]
+  (let [difference (- (count vec2) (count vec1))]
+    (if (<= difference 0) vec1
+      (into [] (concat vec1 (repeat difference "0"))))))
+
+(defn compare-vecs [version1 version2]
+   (map #(cond (> (read-string %1) (read-string %2)) 1
+                (= (read-string %1) (read-string %2)) 0
+                :else -1) version1 version2))
+
+(defn version-compare [version1 version2]
+  (let [version1-vec (version-vec version1)
+        version2-vec (version-vec version2)
+        pad-version1 (pad-vec version1-vec version2-vec)
+        pad-version2 (pad-vec version2-vec version1-vec)
+        compare-list (compare-vecs pad-version1 pad-version2)
+        filtered-list (filter #(not (zero? %)) compare-list)]
+    (if (empty? filtered-list) 0
+      (first filtered-list))))
+
