fc0462ca609d9383fe777d41dc8f50f5a8c771f1
Kirby Bryant <akcbryant@gmail.com>
2016-02-01 16:59:53 -0800

    Going home.

--- spec/kata2/core_spec.clj
+++ spec/kata2/core_spec.clj
@@ -2,3 +2,16 @@
   (:require [speclj.core :refer :all]
             [kata2.core :refer :all]))
 
+(describe "Return a vector of the version number"
+  (it "1"
+    (should= ["1"] (version-vec "1")))
+  (it "1.0.0"
+    (should= ["1" "0" "0"] (version-vec "1.0.0")))
+  (it "2.3.4.5.6"
+    (should= ["2" "3" "4" "5" "6"] (version-vec "2.3.4.5.6"))))
+
+(describe "Compare two vectors of numbers"
+  (it "[1] [1]"
+    (should= 0 (compare-versions ["1"]["1"])))
+  (it "[1 0] [0 0 1]"
+    (should= 1 (compare-versions ["1" "0"]["0" "0" "1"]))))
--- src/kata2/core.clj
+++ src/kata2/core.clj
@@ -1,2 +1,10 @@
 (ns kata2.core
   (:require [clojure.string :as string]))
+
+(defn version-vec [version-str]
+  (string/split version-str #"\."))
+
+(defn compare-versions [version1 version2]
+  (map #(cond (> (read-string %1) (read-string %2)) 1
+              (= (read-string %1) (read-string %2)) 0
+              :else -1) version1 version2))
